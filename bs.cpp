cb3605ebb4c488fb44d28982153e30b5994afc1d
Surabhi Agnihotri <114336295+surabhi-agnihotri@users.noreply.github.com>
2024-02-09 23:25:51 +0530

    Update bs.cpp

--- bs.cpp
+++ bs.cpp
@@ -1,49 +1,90 @@
-// int search(vector<int> &nums, int target) {
-//     int n=nums.size();
-//     for(int i=0;i<n;i++){
+// // int search(vector<int> &nums, int target) {
+// //     int n=nums.size();
+// //     for(int i=0;i<n;i++){
 
-//             if(nums[i]==target)
-//             {
+// //             if(nums[i]==target)
+// //             {
+// //                 return i;
+// //             }
+// //             }
+// //      return -1;
+// // }
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+// int search(vector<int> &nums, int target)
+// {
+
+//     // Write your code here.
+
+//     int mid = nums.size() / 2;
+
+//     if (nums[mid] == target)
+//         return mid;
+
+//     else if (target <= nums[mid])
+//     {
+
+//         for (int i = 0; i < mid; i++)
+//         {
+
+//             if (target == nums[i])
 //                 return i;
-//             }
-//             }
-//      return -1;
+//         }
+//     }
+
+//     else if (target > nums[mid])
+//     {
+
+//         for (int i = mid + 1; i < nums.size(); i++)
+//         {
+
+//             if (target == nums[i])
+//                 return i;
+//         }
+//     }
+
+//     return -1;
 // }
+
+
+
+// optimsed in logn
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
-int search(vector<int> &nums, int target)
+int binarySearch(vector<int> &nums, int target, int low, int high)
 {
 
-    // Write your code here.
-
-    int mid = nums.size() / 2;
-
-    if (nums[mid] == target)
-        return mid;
-
-    else if (target <= nums[mid])
+    if (low > high)
     {
-
-        for (int i = 0; i < mid; i++)
-        {
-
-            if (target == nums[i])
-                return i;
-        }
+        return -1;
     }
 
-    else if (target > nums[mid])
+    int mid = low + (high - low) / 2;
+
+    for (int i = 0; i < nums.size(); i++)
     {
-
-        for (int i = mid + 1; i < nums.size(); i++)
+        if (nums[mid] == target)
         {
+            return mid;
+        }
 
-            if (target == nums[i])
-                return i;
+        else if (nums[mid] > target)
+        {
+            return binarySearch(nums, target, low, mid - 1);
+        }
+
+        else if (nums[mid] < target)
+        {
+            return binarySearch(nums, target, mid + 1, high);
         }
     }
+}
 
-    return -1;
+int search(vector<int> &nums, int target)
+{
+    return binarySearch(nums, target, 0, nums.size() - 1);
 }
