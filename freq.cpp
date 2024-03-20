6f1885ed74cb9275c6031127bc2ce23fccb3be49
Surabhi <surabhi.2125cs1188@kiet.edu>
2024-03-20 22:53:26 +0530

    added: cpp sol for returning string in dec order of characters freq

---
+++ freq.cpp
@@ -0,0 +1,17 @@
+string sortByFrequency(int n, string &s)
+{
+    int low = 0;
+    map<char, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        mp[s[i]]++;
+    }
+    for (auto x : mp)
+    {
+        while (x.second--)
+        {
+            s[low++] = x.first;
+        }
+    }
+    return s;
+}
