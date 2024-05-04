9fb3184eb02b73b88bb5787cca5c4e09fe4db8d7
Surabhi <surabhi.2125cs1188@kiet.edu>
2024-05-04 18:34:39 +0530

    added: cpp sol for checking if given ll is a palindrome or not

---
+++ palinll.cpp
@@ -0,0 +1,58 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node()
+        {
+            this->data = 0;
+            next = NULL;
+        }
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+        Node(int data, Node* next)
+        {
+            this->data = data;
+            this->next = next;
+        }
+    };
+
+*****************************************************************/
+bool checkpalindrome(vector<int> arr)
+{
+    int n = arr.size();
+    int s = 0;
+    int e = n - 1;
+    while (s <= e)
+    {
+        if (arr[s] != arr[e])
+        {
+            return false;
+            break;
+        }
+        else
+        {
+            s++;
+            e--;
+        }
+    }
+    return true;
+}
+bool isPalindrome(Node *head)
+{
+    vector<int> arr;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        arr.push_back(temp->data);
+        temp = temp->next;
+    }
+    return checkpalindrome(arr);
+}
